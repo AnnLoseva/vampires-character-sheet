f09ae2081d0242b8ee10f0db2af0af8645ed4abd
AnnLoseva <ann.loseva03@gmail.com>
2026-05-06 19:36:30 +0300

    /

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export default function VampireSheet() {
+  return (
+    <iframe 
+      src="/sheet.html" 
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        display: "block"
+      }}
+      title="VTM Character Sheet"
+    />
+  );
+}
